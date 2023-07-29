f7830788099f741efd1e73ee164099f690448450
Bryan <bryanagan25@gmail.com>
2023-07-29 13:10:03 +0800

    Improve interaction between buttons

--- buttons.py
+++ buttons.py
@@ -29,20 +29,35 @@ class Buttons:
         self.reset_button = ctk.CTkButton(self.window, text="Reset",
                                           hover_color='blue', width=100, height=75)
         self.reset_button.place(x=305, y=150)
+        self.reset_button.configure(state="disabled")
 
     def start_stopwatch(self):
         self.stop_button.configure(state="normal")
+        self.start_button.configure(state="disabled")
 
         self.display_time_inst.start_time()
 
     def stop_stopwatch(self):
         self.display_time_inst.stop_time()
 
+        # Updates button status
+        self.reset_button.configure(state="normal")
+        self.stop_button.configure(state="disabled")
+
         # Once stop button is pressed, the continue button will replace it
         self.start_button.place_forget()
-        self.continue_button.place(x=35, y=150)
+
+        if not self.continue_button.winfo_ismapped():
+            self.continue_button.place(x=35, y=150)
+        else:
+            self.continue_button.configure(state="normal")
 
     def continue_stopwatch(self):
+        # Updates button status
+        self.reset_button.configure(state="disabled")
+        self.stop_button.configure(state="normal")
+        self.continue_button.configure(state="disabled")
+
         self.display_time_inst.continue_time()
         self.display_time_inst.start_time()
 
