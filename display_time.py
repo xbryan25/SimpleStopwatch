992c9ecb017829967b4813e1afdecdbc457171b0
Bryan <bryanagan25@gmail.com>
2023-08-12 19:48:05 +0800

    Edit time_update, no more if statement, both time_labels get updated

--- display_time.py
+++ display_time.py
@@ -68,7 +68,5 @@ class DisplayTime:
         self.time_label_min.configure(text=f"{self.hour:02d}:{self.minute:02d}:{self.second:02d}")
 
     def time_update(self):
-        if self.time_label_max.winfo_ismapped():
-            self.time_label_max.configure(text=f"{self.hour:02d}:{self.minute:02d}:{self.second:02d}")
-        else:
-            self.time_label_min.configure(text=f"{self.hour:02d}:{self.minute:02d}:{self.second:02d}")
+        self.time_label_max.configure(text=f"{self.hour:02d}:{self.minute:02d}:{self.second:02d}")
+        self.time_label_min.configure(text=f"{self.hour:02d}:{self.minute:02d}:{self.second:02d}")
